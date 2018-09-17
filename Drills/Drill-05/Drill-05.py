1695e8ace4c16ed7e2159ff4b5690019fe0edfa2
ahn-hyung-jun(school) <43131645+ahn-hyung-jun@users.noreply.github.com>
2018-09-17 16:21:22 +0900

    위로 올라가는 함수 제작
    우측 하단에서 우측 상단으로 올라가는 함수를 만듬

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -10,13 +10,21 @@ def move_from_center_to_right():
     y = 90
     while x<800-25:
         clear_canvas_now()
-        grass.draw.now(400,30)
+        grass.draw_now(400,30)
         character.draw_now(x,y)
         x+=2
         delay(0.01)
 
 def move_up():
-    pass
+    x = 800-25
+    y = 90
+    while y<600-50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y += 2
+        delay(0.01)
+    
 
 def move_left():
     pass
@@ -28,7 +36,7 @@ def move_from_left_to_center():
     pass
 
 def make_rectangle():
-    move_from_center_to_right()
+    #move_from_center_to_right()
     move_up()
     move_left()
     move_down()
